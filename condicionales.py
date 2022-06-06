f1c30c9534a9bce902b41e1ac69392d60352217b
AlanGarciaQ <ceinnova08447@innovaccion.mx>
2022-06-06 16:14:51 -0500

    Ejemplo añadido

--- condicionales.py
+++ condicionales.py
@@ -6,6 +6,8 @@ calificacion = int(calificacion)
 # Preguntamos si la calificacion es menor a 700
 if calificacion < 700 : 
     print("Veees, por no estudiar") # Si es menor a 700 muestra esto
+elif calificacion == 700 :
+    print("PANZASOOOOO")
 elif calificacion > 1000 :
     print("MIENTES!!!! No puedes sacar mas de mil")
 else : # Si no se cumple el if anterior pasa a esta linea
